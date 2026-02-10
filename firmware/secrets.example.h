49cffa6a262e03307e7edb69448700e2dd38eb13
Tony Sebastian <tonysebastine@gmail.com>
2026-02-10 14:56:49 +0530

    v0

--- firmware/secrets.example.h
+++ firmware/secrets.example.h
@@ -1,8 +1,8 @@
 #pragma once
 
 // Wi-Fi
-static const char* WIFI_SSID = "YOUR_WIFI_SSID";
-static const char* WIFI_PASSWORD = "YOUR_WIFI_PASSWORD";
+static const char* WIFI_SSID = "w3infotech";
+static const char* WIFI_PASSWORD = "w3@infoch#345";
 
 // MQTT
 static const char* MQTT_HOST = "192.168.1.100";
@@ -15,4 +15,11 @@ static const char* MQTT_CLIENT_ID = "m5-door-sensor";
 static const bool TELEGRAM_ENABLED = false;
 static const char* TELEGRAM_BOT_TOKEN = "";
 static const char* TELEGRAM_CHAT_ID = "";
+
+// Static IP (optional)
+static const bool USE_STATIC_IP = true;
+static const IPAddress STATIC_IP_ADDRESS(192, 168, 1, 100);
+static const IPAddress STATIC_GATEWAY(192, 168, 1, 1);
+static const IPAddress STATIC_SUBNET(255, 255, 255, 0);
+static const IPAddress STATIC_DNS(8, 8, 8, 8); // Google DNS
 
